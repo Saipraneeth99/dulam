e7975b126ddaec0adf85d0bae6ffef9484391c41
Saipraneeth99 <saipraneeth.dulam@gmail.com>
2021-05-19 01:28:38 +0530

    String Interpolation, Property Binding and Double binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent} from './posts/post-create/post-create.component';
@@ -11,7 +11,8 @@ import { PostCreateComponent} from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,21 @@ import { Component } from '@angular/core';
   templateUrl:'./post-create.component.html'
 })
 export class PostCreateComponent{
+  newPost='No content'
+  newPostWithData=''
+  douleBinding=''
+  onAddPost()
+  {
+    this.newPost='User\'s post'
+    alert("Post added")
+  }
+  onAddPostwithData(inputPost : HTMLTextAreaElement)
+  {
+    this.newPostWithData=inputPost.value;
+  }
+  onDoubleBinding()
+  {
+    this.douleBinding=''
+  }
 
 }
